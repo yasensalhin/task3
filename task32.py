494eeddea8d99293a96b98c2dcaa1c1d02d16c8c
yasensalhin <yasensalhin123@gmail.com>
2025-09-08 01:08:55 +0300

    Update task32.py

--- task32.py
+++ task32.py
@@ -6,26 +6,15 @@ img =cv2.imread('shapes.jpg')
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 out =img_rgb.copy()
 
-lower_red = np.array([100, 0, 0])
-higher_red = np.array([255, 80, 80])
 
-lower_blue=np.array([0,0,100])
-higher_blue=np.array([100,100,255])
-
-lower_black = np.array([0, 0, 0])
-higher_black = np.array([50, 50, 50])
-
-mask_blue =cv2.inRange(img_rgb,lower_blue,higher_blue)
-
-mask_red=cv2.inRange(img_rgb,lower_red,higher_red)
-
-mask_black=cv2.inRange(img_rgb,lower_black,higher_black)
-print(mask_blue.shape)
+red_mask   = (out[...,0] > 200) & (out[...,1] < 80)  & (out[...,2] < 80)
+blue_mask  = (out[...,2] > 200) & (out[...,0] < 80)  & (out[...,1] < 80)
+black_mask = (out[...,0] < 60)  & (out[...,1] < 60)  & (out[...,2] < 60)
 
 
-out[mask_blue>0]=[0,0,0]
-out[mask_red>0]=[0,0,255]
-out[mask_black>0]=[255,0,0]
+out[red_mask]=[0,0,255]
+out[blue_mask]=[0,0,0]
+out[black_mask]=[255,0,0]
 
 
 fig, axes = plt.subplots(1, 2)
@@ -38,3 +27,4 @@ axes[1].set_title('Processed Image')
 axes[1].axis('off')
 
 plt.show()
+
